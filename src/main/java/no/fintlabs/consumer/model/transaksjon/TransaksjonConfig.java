30066fee6a79b3e4802c260bd8f0572a87b17cab
Hknots <Hknots99@gmail.com>
2023-11-14 13:26:56 +0100

    fix sharedprops reference

--- src/main/java/no/fintlabs/consumer/model/transaksjon/TransaksjonConfig.java
+++ src/main/java/no/fintlabs/consumer/model/transaksjon/TransaksjonConfig.java
@@ -1,7 +1,7 @@
 package no.fintlabs.consumer.model.transaksjon;
 
 import no.fint.model.resource.okonomi.regnskap.TransaksjonResource;
-import no.fintlabs.core.consumer.shared.ConsumerProps;
+import no.fintlabs.core.consumer.shared.config.ConsumerProps;
 import no.fintlabs.core.consumer.shared.resource.ConsumerConfig;
 import org.springframework.stereotype.Component;
 
